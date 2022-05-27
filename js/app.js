a9c3f1e85bbe960696ffa404fcba3dc31ab1d9be
StefanoSax93 <101599907+StefanoSax93@users.noreply.github.com>
2022-05-27 16:47:20 +0200

    aggiunta header + tabella

--- js/app.js
+++ js/app.js
@@ -7,3 +7,46 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.*/
+
+const gridContainer = document.querySelector(".grid-container");
+
+function createGrid(xCells, yCells) {
+  // quantità di celle da creare
+  const cellsNumber = xCells * yCells;
+
+    console.log(cellsNumber);
+
+  gridContainer.style.width = `calc(var(--cell-size) * ${xCells})`;
+
+  // variabile dove inserire i numeri random generati
+const numeriGenerati = [];
+let randomNumber=0;
+  // creo ogni singola cella necessaria
+for (let i = 0; i < cellsNumber; i++) {
+    randomNumber += 1;
+    numeriGenerati.push(randomNumber);
+    // creo un div che rappresenta una singola cella
+    const cell = document.createElement("div");
+    // aggiungo le classi per stilizzare la cella
+    cell.classList.add("cell","d-flex","justify-content-center","align-items-center");
+    // inserisco il numero all interno della cella
+    cell.innerHTML = `<span>${randomNumber}</span>`;
+
+    // Aggiungo un event listener sul click della cella
+    cell.addEventListener("click", function () {
+      // this = l'elemento che ha generato l'evento usato nel addEventListener
+    console.log("hai cliccato il numero", this.innerText);
+
+    const numero = parseInt(this.innerText);
+
+    if (numero) {
+        this.classList.add("bg-secondary","text-white");
+    } 
+    
+    });
+
+    gridContainer.append(cell);
+}
+}
+
+createGrid(10, 10);
